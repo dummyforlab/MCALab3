ac5ed07a68c4ca0c0286aaa40b74e2e8d2c9ce1d
dummyforlab <iron2093man@gmail.com>
2024-08-26 23:49:12 +0530

    Update Problem1.py

--- Week2/Problem1.py
+++ Week2/Problem1.py
@@ -1,9 +1,12 @@
 def digitsReverse(number):
-original=number
-print()
-print("The digits in reverse order are : ")
-while original>0:
-print(original%10)
-original=original//10
+    original=number
+    print()
+    print("The digits in reverse order are : ")
+    while original>0:
+        print(original%10)
+        original=original//10
+
+
 number=int(input("Enter a number : "))
+
 digitsReverse(number)

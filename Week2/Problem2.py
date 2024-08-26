edc2f21e8597644d2f8c32b1e393b20e95da6edc
dummyforlab <iron2093man@gmail.com>
2024-08-26 23:48:45 +0530

    Update Problem2.py

--- Week2/Problem2.py
+++ Week2/Problem2.py
@@ -1,1 +1,11 @@
+def countDigits(number):
+    count=0
+    original=number
+    while original>0 :
+        original=original//10
+        count+=1
 
+    return count
+
+number=int(input("Enter a number : "))
+print(f"Total digits in {number} are {countDigits(number)}")

b0800d6c6fb7fe62351704e3192da324fd90d5ac
dummyforlab <iron2093man@gmail.com>
2025-07-19 11:56:31 +0530

    Create Problem3.py

---
+++ Week1/Problem3.py
@@ -0,0 +1,8 @@
+
+numbers = [12, 25, 30, 44, 50, 67, 70, 15, 19, 20, 55, 62, 40, 99, 85, 10, 5, 60, 35, 45]
+
+# Iterate and print numbers divisible by 5
+print("Numbers divisible by 5:")
+for num in numbers:
+    if num % 5 == 0:
+        print(num)
